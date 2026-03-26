15d124a05b8492ea14fb634354301ec4f1d6485d
Jamie Hilditch <hilditch@stanford.edu>
2026-03-26 10:31:12 -0700

    drop attrs from time

--- src/ucla_roms_post_processing/time.py
+++ src/ucla_roms_post_processing/time.py
@@ -11,5 +11,6 @@ def make_ocean_time_dim(ds: xr.Dataset) -> xr.Dataset:
 def add_time_coordinate(ds: xr.Dataset, origin: np.datetime64) -> xr.Dataset:
     """Add time coordinate to the dataset."""
     time = origin + np.timedelta64(int(1e9), 'ns') * ds["ocean_time"]
+    time = time.drop_attrs()
     ds = ds.assign_coords(time=time)
     return ds
